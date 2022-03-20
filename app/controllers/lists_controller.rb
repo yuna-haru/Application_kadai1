7e9f1d1de80ee0b4cdce6d01ee6563e7f7540495
EC2 Default User <ec2-user@ip-172-31-35-197.ec2.internal>
2022-03-20 14:07:34 +0000

    application kadai view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,27 @@
 class ListsController < ApplicationController
   def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new
+    list.save
+    redirect_to '/'
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
